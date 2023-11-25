ae13c6709f751d69c270d7f48e9cf0e68096e0e3
bluestarburst <hargreaves.bryant@gmail.com>
2023-11-25 13:38:52 -0600

    print test

--- train.py
+++ train.py
@@ -382,6 +382,9 @@ def main(
                     if accelerator.sync_gradients:
                         accelerator.clip_grad_norm_(unet.parameters(), max_grad_norm)
                         
+                    for param in unet.parameters():
+                        print(param.grad)
+                        
                     optimizer.step()
                     lr_scheduler.step()
                     optimizer.zero_grad()
