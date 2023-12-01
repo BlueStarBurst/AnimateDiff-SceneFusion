470858d0597a4ff073c092a2ba5130b3fb0fded2
Bryant Hargreaves <hargreaves.bryant@gmail.com>
2023-11-30 20:55:16 -0600

    adhoc

--- server.py
+++ server.py
@@ -254,4 +254,4 @@ def index():
 
 # run the app
 if __name__ == '__main__':
-    app.run(port=5000, host="0.0.0.0", ssl_context=('cert.pem', 'key.pem'))
+    app.run(port=5000, host="0.0.0.0", ssl_context="adhoc")
